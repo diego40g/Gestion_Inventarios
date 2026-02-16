2e0c6d4b5ebdebcafbc71e3aa58dfa50f3575fa8
diego40g <diegopaznaula@hotmail.com>
2026-02-15 22:22:09 -0500

    feat: Add initial CLI and menu classes for inventory management system

---
+++ inventario/src/main/java/ec/edu/istpet/inventario/presentation/helpers/Formatters.java
@@ -0,0 +1,5 @@
+package ec.edu.istpet.inventario.presentation.helpers;
+
+public class Formatters {
+
+}
---
+++ inventario/src/main/java/ec/edu/istpet/inventario/presentation/menus/MenuPrincipal.java
@@ -0,0 +1,5 @@
+package ec.edu.istpet.inventario.presentation.menus;
+
+public class MenuPrincipal {
+
+}
