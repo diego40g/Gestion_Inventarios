3fdf6747dec7b72107c8db7d7e40549babea6e3c
diego40g <diegopaznaula@hotmail.com>
2026-02-15 22:21:46 -0500

    feat: Add initial classes and interfaces for discount strategies and services in inventory management

---
+++ inventario/src/main/java/ec/edu/istpet/inventario/application/estrategias/DescuentoProgresivo.java
@@ -0,0 +1,5 @@
+package ec.edu.istpet.inventario.application.estrategias;
+
+public class DescuentoProgresivo {
+
+}
---
+++ inventario/src/main/java/ec/edu/istpet/inventario/application/servicios/VentaService.java
@@ -0,0 +1,5 @@
+package ec.edu.istpet.inventario.application.servicios;
+
+public class VentaService {
+
+}
